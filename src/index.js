662fa6f93b5f57b1341c35bf52bc40ca9fedab61
NethmiRodrigo <34070216+NethmiRodrigo@users.noreply.github.com>
2020-02-03 15:40:26 +0530

    Created read stream from file

--- src/index.js
+++ src/index.js
@@ -9,16 +9,9 @@ const rl = readline.createInterface({
 let file;
 
 rl.question('Input file name ', (answer) => {
-  // TODO: Log the answer in a database
-  console.log(`Reading file name: ${answer}`);
-  file=answer;
-  try {  
-    var data = fs.readFileSync(`../test_data/${file}.txt`, 'utf8');
-    console.log(typeof data);
-    console.log(data); 
-} catch(e) {
-    console.log('Error:', e.stack);
-}
+
+    console.log(`Reading file name: ${answer}`);
+    file=answer;
+    var readStream = fs.createReadStream(`../test_data/${file}.txt`, 'utf8');
   rl.close();
 });
-
