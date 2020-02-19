42dd5decfed7f498eda24a931498174032f1315f
NethmiRodrigo <34070216+NethmiRodrigo@users.noreply.github.com>
2020-02-20 00:22:16 +0530

    Fixed bug

--- src/horizontalSlideshow.js
+++ src/horizontalSlideshow.js
@@ -11,29 +11,21 @@ const makeSlideshow = file => {
   for (let i = 0; i < sortedImages.length; i++) {
     let score = 0;
     let maxScore = 0;
-    if (i === 0) {
-      slide.push(sortedImages[i]);
-      continue;
-    } else {
-      for (j = i + 1; j < sortedImages.length; j++) {
-        console.log(sortedImages[i]);
-        console.log(sortedImages[j].tags);
-        score = calScore(sortedImages[i].tags, sortedImages[j].tags);
-        // if (sortedImages[j].numberOfTags < 2 * maxScore) {
-        //   continue;
-        // }
-        if (score > maxScore) {
-          maxScore = score;
-          selectedImageIndex = j;
-          continue;
-        }
+    for (j = i + 1; j < sortedImages.length; j++) {
+      if (sortedImages[j].numberOfTags < 2 * maxScore) {
+        continue;
       }
-      slide.push(sortedImages[selectedImageIndex]);
-      totalScore = totalScore + maxScore;
-      let selectedImage = sortedImages.splice(selectedImageIndex, 1);
-      sortedImages.splice(i + 1, 0, selectedImage);
-      console.log("current score", totalScore);
+      score = calScore(sortedImages[i].tags, sortedImages[j].tags);
+      if (score > maxScore) {
+        maxScore = score;
+        selectedImageIndex = j;
+        continue;
+      }
     }
+    totalScore = totalScore + maxScore;
+    let selectedImage = sortedImages.splice(selectedImageIndex, 1);
+    sortedImages.splice(i + 1, 0, selectedImage[0]);
+    console.log("current score", totalScore);
   }
   console.log(totalScore);
 };
