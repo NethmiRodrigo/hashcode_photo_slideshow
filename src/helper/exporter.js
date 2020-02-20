c28f5b5b4ec8e7f77443a2ad2263d21c4f92b67d
NethmiRodrigo <34070216+NethmiRodrigo@users.noreply.github.com>
2020-02-20 18:09:42 +0530

    Fixed exporter

--- src/helper/exporter.js
+++ src/helper/exporter.js
@@ -1,23 +1,16 @@
 const fs = require("fs");
 
 module.exports = (filename, slides) => {
-  fs.appendFile(`../output/${filename}.out`, slides.length + "\n", function(
-    error
-  ) {
-    if (error) return console.log(error);
-
-    for (let i = 0; i < slides.length; i++) {
-      let line = [];
-      for (let j = 0; j < slides[i].images.length; j++) {
-        line.push(slides[i].images[j].id);
-      }
-      fs.appendFile(
-        `../output/${filename}.out`,
-        line.join(" ") + "\n",
-        function(error) {
-          if (error) return console.log(error);
-        }
-      );
-    }
+  const stream = fs.createWriteStream(`../output/${filename}.out`, {
+    flags: "a"
   });
+  stream.write(slides.length + "\n");
+  for (let i = 0; i < slides.length; i++) {
+    let line = [];
+    for (let j = 0; j < slides[i].images.length; j++) {
+      line.push(slides[i].images[j].id);
+    }
+    stream.write(line.join(" ") + "\n");
+  }
+  stream.end();
 };
